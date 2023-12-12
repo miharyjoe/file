cf2db30cd558b9f0aeecb71ddfc2a459cf617e79
miharyjoel <miharyjoel@gmail.com>
2023-12-12 17:00:50 +0300

    feat: init project

---
+++ src/main/java/org/handling/handlingerror/HandlingErrorApplication.java
@@ -0,0 +1,13 @@
+package org.handling.handlingerror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingErrorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HandlingErrorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/handling/handlingerror/HandlingErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.handling.handlingerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlingErrorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
