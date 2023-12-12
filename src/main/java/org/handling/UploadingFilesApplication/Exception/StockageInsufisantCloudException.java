7a862af85d397196bb75c41a6cad64200b56f0da
miharyjoel <miharyjoel@gmail.com>
2023-12-12 20:58:53 +0300

    feat: add updload and dowload the file

---
+++ src/main/java/org/handling/UploadingFilesApplication/Exception/StockageInsufisantCloudException.java
@@ -0,0 +1,7 @@
+package org.handling.UploadingFilesApplication.Exception;
+
+public class StockageInsufisantCloudException extends RuntimeException{
+  public StockageInsufisantCloudException(String message) {
+    super(message);
+  }
+}
--- src/test/java/org/handling/UploadingFilesApplication/HandlingErrorApplicationTests.java
+++ src/test/java/org/handling/UploadingFilesApplication/HandlingErrorApplicationTests.java
@@ -1,4 +1,4 @@
-package org.handling.handlingerror;
+package org.handling.UploadingFilesApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
